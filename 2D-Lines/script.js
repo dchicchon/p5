b182a0239adb908e0af66c5035a22eebadf0e680
dchicchon <danielchicchon@gmail.com>
2022-02-03 16:29:40 -0500

    edit code for 2D-lines

--- 2D-Lines/script.js
+++ 2D-Lines/script.js
@@ -1,38 +1,43 @@
-const { innerWidth, innerHeight } = window
+const { innerWidth: width, innerHeight: height } = window
 
 function setup() {
-    createCanvas(innerWidth, innerHeight)
+    createCanvas(width, height)
 }
 
-let xPos = innerWidth / 2
-let yPos = innerHeight / 2
+const startingX = width / 10 // starting x position
+const startingY = height / 10 // starting y position
+const xNum = 100 // number of lines x
+const yNum = 50 // number of lines y 
+const spacingFactor = 15 // spaces out the lines
+const timingFunction = 5 // changes the timing of making the lines
+const lineThickness = 7 // thickness of lines
+const lineLength = 25 // line length
+const lineIncrement = 2; // determines how much a line grows
+const SLEEP = false; // on/off to see line growth or no growth
+const RANDOM_LENGTH = false; // use random line length
+const LOOP = false // let loop or not
 
-// lines
-// line(x1, y1, x2, y2)
-function draw() {
+async function draw() {
+    if (!LOOP) noLoop()
     background(0);
-    stroke('yellow')
-    strokeWeight(10)
-    // we want this line to wrap itself like a snake 
-    // we can make a line loop by keeping track of the previous xPos and yPos
-    translate(-xPos, -yPos)
-    // columns
-    for (let k = 0; k < width / 50; k++) {
-        // rows
-        for (let j = 0; j < height / 50; j++) {
-            push()
-            let xCurr = xPos;
-            let yCurr = yPos;
-            translate(k * 50, j * 50)
-            for (let i = 0; i < 10; i++) {
-                let xNext = j % 2 === 0 ? xCurr + 15 : xCurr - 15;
-                let yNext = yCurr + 25
-                line(xCurr, yCurr, xNext, yNext)
-                xCurr += 25;
-                yCurr += 0;
+    for (let k = 0; k < xNum; k++) { // row
+        let xCurrent = (k * spacingFactor) + (startingX); // start from center x
+        for (let j = 0; j < yNum; j++) { // col
+            let yCurrent = (j * spacingFactor) + (startingY); // start from center y
+            let randomNum = floor(random() * 5) // get random number
+            let color = colors(randomNum, 5) // get color
+            let length = RANDOM_LENGTH ? floor((random() * lineLength) + 10) : lineLength // length
+            if (j % 2 !== 0) { 
+                // make this squiggly by using a sin function?
+                for (let i = 0; i < length; i += lineIncrement) {
+                    let xNext = xCurrent + (i);
+                    let yNext = yCurrent - (i);
+                    stroke(color)
+                    strokeWeight(lineThickness)
+                    if (SLEEP) await sleep(timingFunction)
+                    line(xCurrent, yCurrent, xNext, yNext)
+                }
             }
-            pop()
         }
     }
-
 }
--- helpers/helper.js
+++ helpers/helper.js
@@ -17,6 +17,17 @@ function inBounds(x, y) {
     return (x > 0 && x < width && y > 0 && y < height)
 }
 
+// current num, end num
+function colors(currentNum, endNum, colorsToUse) {
+    let colors = ['cyan', 'violet', 'yellow', 'lightgreen', 'orange']
+    if (endNum > colors.length) throw "Color function: end number is too long!"
+    return colors[currentNum % endNum]
+}
+
+function rgb(r, g, b) {
+    return `rgb(${r},${g},${b})`
+}
+
 async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
